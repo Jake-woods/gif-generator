19e800f1ee504a1599a06a61a73cacfe5fb99e30
Jake-woods <jake.13.5.2016@gmail.com>
2019-01-04 11:52:01 +0000

    Basic search behaviour commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const form = document.querySelector('form');
+const returnedImg = document.querySelector('#image');
+
+
+// Prevent page from reloading
+form.addEventListener('submit', (e) => {
+   e.preventDefault();
+   const inputSearch = document.querySelector('#user-search').value;
+   // URL structure
+   const giphy = {
+      api: 'http://api.giphy.com/v1/gifs/search?',
+      apiKey: '&api_key=KvlI7qrbEwAVr36bcLHvtPTSMZItjtnu',
+      search: `&q=${inputSearch}`
+   }
+
+   // Source for image form search value
+   const url = giphy.api + giphy.apiKey + giphy.search;
+
+   // Fetch data on search and change image source
+   fetch(url)
+      .then(res => res.json())
+      .then(data => {
+         returnedImg.setAttribute('src', data.data[0].images.downsized.url);
+      });
+
+   // Reset form input
+   form.reset();
+});
