86c8bd9b23b94f7e95570be771cafc4544eb10af
Jake-woods <jake.13.5.2016@gmail.com>
2019-01-04 12:10:01 +0000

    Change behaviour to random gif on search

--- js/script.js
+++ js/script.js
@@ -1,26 +1,25 @@
 const form = document.querySelector('form');
 const returnedImg = document.querySelector('#image');
 
-
-// Prevent page from reloading
 form.addEventListener('submit', (e) => {
    e.preventDefault();
    const inputSearch = document.querySelector('#user-search').value;
+
    // URL structure
    const giphy = {
-      api: 'https://api.giphy.com/v1/gifs/search?',
-      apiKey: '&api_key=KvlI7qrbEwAVr36bcLHvtPTSMZItjtnu',
-      search: `&q=${inputSearch}`
+      api: 'https://api.giphy.com/v1/gifs/random?',
+      apiKey: 'api_key=KvlI7qrbEwAVr36bcLHvtPTSMZItjtnu&tag=',
+      search: `${inputSearch}`
    }
 
    // Source for image form search value
    const url = giphy.api + giphy.apiKey + giphy.search;
-
+   console.log(url)
    // Fetch data on search and change image source
    fetch(url)
       .then(res => res.json())
       .then(data => {
-         returnedImg.setAttribute('src', data.data[0].images.downsized.url);
+         returnedImg.setAttribute('src', data.data.images.downsized.url);
       });
 
    // Reset form input
