3e85bb66f69b7bae619d8e507eb3b35423bf97bc
Jake-woods <jake.13.5.2016@gmail.com>
2019-01-04 12:34:26 +0000

    Change behaviour for search input

--- js/script.js
+++ js/script.js
@@ -1,27 +1,30 @@
 const form = document.querySelector('form');
 const returnedImg = document.querySelector('#image');
 
+
+// Prevent page from reloading
 form.addEventListener('submit', (e) => {
    e.preventDefault();
    const inputSearch = document.querySelector('#user-search').value;
 
    // URL structure
    const giphy = {
-      api: 'https://api.giphy.com/v1/gifs/random?',
-      apiKey: 'api_key=KvlI7qrbEwAVr36bcLHvtPTSMZItjtnu&tag=',
-      search: `${inputSearch}`
+      api: 'https://api.giphy.com/v1/gifs/search?',
+      apiKey: '&api_key=KvlI7qrbEwAVr36bcLHvtPTSMZItjtnu',
+      search: `&q=${inputSearch}`
    }
 
    // Source for image form search value
    const url = giphy.api + giphy.apiKey + giphy.search;
-   console.log(url)
+
    // Fetch data on search and change image source
    fetch(url)
       .then(res => res.json())
       .then(data => {
-         returnedImg.setAttribute('src', data.data.images.downsized.url);
-      });
+         const randomImg = Math.floor(Math.random() * data.data.length);
 
+         returnedImg.setAttribute('src', data.data[randomImg].images.downsized.url);
+      });
    // Reset form input
    form.reset();
 });
